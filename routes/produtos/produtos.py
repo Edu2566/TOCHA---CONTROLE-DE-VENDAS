485619728943767b27d0a5f4170b0a1d49b7ead2
Edu_Santos2566 <eduardocorral296@gmail.com>
2024-06-11 10:19:51 -0300

    Feature: Product Update Function 11062024
    Crete the Update function on the product list and your style. Delete the listaProduto.css and listaCliente.css and merge them in listaStyle.css.

--- routes/produtos/produtos.py
+++ routes/produtos/produtos.py
@@ -39,3 +39,60 @@ def listaProduto():
         cursor.close()
 
     return render_template('listaProduto.html', produto=produto)
+
+#Função de deletar dado
+@produto_blueprint.route('/deletar-produto/<int:id_produto>', methods=['POST'])
+def deletarProduto(id_produto):
+    conn = mysql.connection
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("DELETE FROM produtos WHERE id_produto = %s", (id_produto,))
+        conn.commit()
+        return redirect(url_for('produto.listaProduto'))
+    except Exception as e:
+        conn.rollback()
+        return f"Erro ao deletar produto: {str(e)}"
+    finally:
+        cursor.close()
+
+@produto_blueprint.route('/editar-produto/<int:id_produto>', methods=['GET'])
+def editarProduto(id_produto):
+    conn = mysql.connection
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM produtos WHERE id_produto = %s", (id_produto,))
+        produto = cursor.fetchone()
+        if produto:
+            # Formatar o preço para exibir com vírgula
+            preco_br = "{:.2f}".format(produto[2]).replace('.', ',')
+            produto = list(produto)  # Convertendo o tuple para lista
+            produto[2] = preco_br  # Substituindo o preço formatado
+            return render_template('editarProduto.html', produto=produto)
+        else:
+            return "Produto não encontrado."
+    except Exception as e:
+        return f"Erro ao buscar produto: {str(e)}"
+    finally:
+        cursor.close()
+
+@produto_blueprint.route('/atualizar-produto/<int:id_produto>', methods=['POST'])
+def atualizarProduto(id_produto):
+    nome = request.form['nome-produtoform']
+    preco_br = request.form['preco-produtoform']
+    quantidade = request.form['quantidade-produtoform']
+    descricao = request.form['descricao-produtoform']
+
+    # Convertendo o preço para o formato americano
+    preco = float(preco_br.replace(',', '.'))
+
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE produtos SET nome_produto = %s, preco = %s, quantidade = %s, descricao_produto = %s WHERE id_produto = %s", (nome, preco, quantidade, descricao, id_produto))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('produto.listaProduto'))
+    except Exception as e:
+        return f"Erro ao atualizar produto: {str(e)}"
+    
